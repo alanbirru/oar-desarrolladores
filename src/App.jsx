5ad62472ce2f589acb40b34f2a9f5581fc25f063
alanbirru <alanb_99@hotmail.com>
2023-12-18 11:08:46 -0700

    agreganto objetos barra de navegacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Navbar from "./Components/Navbar";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+const navLinks = [
+  { id: "inicio", title: "Inicio" },
+  { id: "mis-proyectos", title: "Mis Proyectos" },
+  { id: "valor", title: "Valor" },
+  { id: "nosotros", title: "Nosotros" },
+  { id: "detalles", title: "Detalles" },
+  { id: "contacto", title: "Contacto" },
+];
