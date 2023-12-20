bfcf774ac29d116cd35d4c9f62a14b5bd2ab1491
alanbirru <alanb_99@hotmail.com>
2023-12-19 17:48:43 -0700

    importando componentes a App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
 import Navbar from "./Components/Navbar";
+import inicio from "./Components/Inicio";
+import NuestrasAlanzas from "./Components/NuestrasAlianzas";
+import MisProyectos from "./Components/MisProyectos";
+import Galeria from "./Components/Galeria";
+import Nosotros from "./Components/Nosotros";
+import Detalles from "./Components/Detalles";
+import VisionMision from "./Components/VisionMision";
+import Reseñas from "./Components/Reseñas";
+import Contacto from "./Components/Contacto";
+import footer from "./Components/Footer";
 
 const App = () => {
   return <div></div>;
